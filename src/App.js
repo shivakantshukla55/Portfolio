c8a9251af9250330910c2e8dc3e36450709eab08
SHIVAKANT SHUKLA <shivakantshukla55@gmail.com>
2022-05-11 22:23:53 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import * as React from "react";
+import { AppRoutes } from "./router";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <AppRoutes />
     </div>
   );
 }
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const About = () => <h2>About</h2>;
+const Work = () => <h2>Work</h2>;
+const Contact = () => <h2>Contact</h2>;
+
+const Home = () => {
+  return <Navigate to="/about" />;
+};
+
+const PageNotFound = () => <h2>Page Not Found</h2>;
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route element={<PageNotFound />} />
+    </Routes>
+  );
+};
